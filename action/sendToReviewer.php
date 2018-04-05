032d6abe86fa7c05b856f45f278f1d5f3d787d7e
Behnam <behnam78@gmail.com>
2018-04-05 10:53:40 -0700

    send to review fixed

--- action/sendToReviewer.php
+++ action/sendToReviewer.php
@@ -1,21 +1,15 @@
 <?php
-require_once("connection.php");
-	
-	$id = $_POST["id-number"];
-	$datetime = date_create_from_format('m#d#Y h a', $_POST["date"]);
-	$date = date_format($datetime, 'Y-m-d H:i:s');
-	$type = $_POST["type"];
-	$category = $_POST["category"];
-	$title = $_POST["title"];
-	$description = $_POST["description"];
-	
-	$query = "UPDATE `opportunities` SET `final_filing_date`='$date', `type`='$type', `category`='$category', `title`='$title', `description`='$description', `status` = 'In Review'  WHERE `id` = '$id'";
-	$result = mysqli_query($bd, $query);
-	if(!$result){
-		echo mysqli_error($bd); 
-	}
-	else{
-        header("Location: ../home.php");
+        $id = $_POST["id-number"];
+		$datetime = date_create_from_format('m#d#Y h a', $_POST["date"]);
+		$date = date_format($datetime, 'Y-m-d H:i:s');
+		$type = $_POST["type"];
+		$category = $_POST["category"];
+		$title = $_POST["title"];
+		$description = $_POST["description"];
+        require_once("connection.php");
+        $id=$_SESSION['id'];
+        $query = "UPDATE `opportunities` SET `final_filing_date`='$date', `type`='$type', `category`='$category', `description`='$description', `title`='$title',  `status` = 'In Review'  WHERE `id` = '$id'";
+        mysqli_query($bd, $query);
         unset($_SESSION['id']);
-	}
+        header("Location: ../home.php");
 ?>
