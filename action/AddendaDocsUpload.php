fc41b61ae92bc99b115e76f8a9e088a9451f5974
Tyler Donaldson <donaldst84@gmail.com>
2018-04-24 18:39:43 -0700

    Fixed a bug in file submission

--- action/AddendaDocsUpload.php
+++ action/AddendaDocsUpload.php
@@ -1,24 +1,24 @@
 <?php 
 require_once('connection.php');
 if($_FILES){
-    $query = "INSERT INTO `submissions` ()";
+    $opportunity_id = $_SESSION['bidDocs'];
+    $query = "INSERT INTO `submissions` (bidder_id, opportunity_id) VALUES (".$_SESSION['SESS_MEMBER_ID'].", ".$opportunity_id.")";
     $result = mysqli_query($bd, $query);
     if($result){
         $submission_id = mysqli_insert_id($bd);
     }
     else{
-        echo "Submission insert failed";
+        echo "Submission insert failed: " . mysqli_error($bd);
     }
     $count = count($_FILES['file']['name']);
     $subheading=$_POST['subheading'];
     $i= 0;
-    while ($i<$count)
+    for ($i; $i<$count; $i++)
      {
         $name= $_FILES['file']['name'][$i];
         $size= $_FILES['file']['size'][$i];
         $type= $_FILES['file']['type'][$i];
         $tmp_name= $_FILES['file']['tmp_name'][$i];
-        $opportunity_id = $_SESSION['bidDocs'];
         $position= strpos($name, ".");  
         
         $fileextension= substr($name, $position + 1);
@@ -31,7 +31,6 @@ if($_FILES){
         $filesPath= "Uploads/$name";
         if (!empty($name)){
         if (move_uploaded_file($tmp_name, $path.$name)) {
-            $i++;
         $query = "INSERT INTO `submission_docs` (`filename`,`directory`,`subheading`, `filetype`, `filesize`, `submission_id`)
 			VALUES('".$name."', '".$filesPath."'  ,'".$subheading[$i]."','".$type."', '".$size."', '".$submission_id."')";
 	
