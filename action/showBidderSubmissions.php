862eec01280265125163d3bf30b8fdc04140c692
Tyler Donaldson <donaldst84@gmail.com>
2018-05-10 16:58:00 -0700

    Added My Submissions tab for bidder & fixes  - Fixed dates to display more readable format  * Minor fix to time format

--- action/showBidderSubmissions.php
+++ action/showBidderSubmissions.php
@@ -1,24 +1,23 @@
 <?php
 
 // DB table to use
-$table = 'opportunities';
- 
+$table = 'submission_info';
+
 // Table's primary key
 $primaryKey = 'id';
- 
+
 // Array of database columns which should be read and sent back to DataTables.
 // The `db` parameter represents the column name in the database, while the `dt`
 // parameter represents the DataTables column identifier. In this case simple
 // indexes
 $columns = array(
-    array( 'db' => 'title', 'dt' => 0 ),
-    array( 'db' => 'category',  'dt' => 1 ),
-    array( 'db' => 'status', 'dt' => 2 ),
-    array( 'db' => 'final_filing_date',  'dt' => 3 ),
-    array( 'db' =>  'last_updated', 'dt' => 4),
-    array( 'db' => 'id', 'dt' => 5)
+    array( 'db' => 'id', 'dt' => 0 ),
+    array( 'db' => 'title', 'dt' => 1 ),
+    array( 'db' => 'status', 'dt' => 2),
+    array( 'db' => 'last_updated', 'dt' => 3)
+
 );
- 
+
 // SQL server connection information
 $sql_details = array(
     'user' => 'root',
@@ -27,11 +26,11 @@ $sql_details = array(
     'host' => 'localhost'
 );
 
- 
+
 require( 'ssp.class.php' );
-$where = "`status`=".'"Posted"';
+
 echo json_encode(
-    SSP::simple( $_GET, $sql_details, $table, $primaryKey, $columns,$where)
+    SSP::simple( $_GET, $sql_details, $table, $primaryKey, $columns )
 );
 
 ?>
